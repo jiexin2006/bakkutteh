acbfeee208a1239ef24c9ee9ad3d1e787b0e9764
Kan_hy <237968832+hyjanesa88@users.noreply.github.com>
2026-04-25 01:56:01 +0800

    Calculate Bitcoin price in MYR and update JSON output
    Updated the fetch_live_bitcoin function to calculate the Bitcoin price in MYR and include it in the output JSON.

--- live_data.py
+++ live_data.py
@@ -6,15 +6,20 @@ def fetch_live_bitcoin():
     print("[SYSTEM] Connecting to global markets...")
     try:
         btc = yf.Ticker("BTC-USD")
+        btc_usd_price = btc.fast_info['last_price']
+
+        # live Exchange Rate (USD to MYR)
+        exchange = yf.Ticker("MYR=X") 
+        usd_to_myr_rate = exchange.fast_info['last_price']
         
-        # Pull the absolute most recent market price using the fast_info method
-        live_price = btc.fast_info['last_price']
+        btc_myr_price = btc_usd_price * usd_to_myr_rate
         
-        # Package it into a clean JSON 
         crypto_state = {
             "asset": "Bitcoin",
             "symbol": "BTC",
-            "current_price_usd": round(live_price, 2),
+            "current_price_usd": round(btc_usd_price, 2),
+            "exchange_rate_usd_myr": round(usd_to_myr_rate, 4),
+            "current_price_myr": round(btc_myr_price, 2),
             "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "market_status": "Live"
         }
